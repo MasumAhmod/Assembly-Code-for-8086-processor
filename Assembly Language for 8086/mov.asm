7f0179add92d9917d5e4da826a20aa96fbc4aada
MasumAhmod <masumahmod332@gmail.com>
2025-12-25 22:29:00 +0600

    Delete Assembly Language for 8086 directory

--- Assembly Language for 8086/mov.asm
+++
@@ -1,8 +0,0 @@
-MOv al,5
-;AL register now have value 5.
-
-MOV BL,Al
-;Bl register now have copy of value AL register  
-
-mov cH,bl
-;Copy the contents of BL (low part of BX) into CH (high part of CX).
